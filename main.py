92e4704564b8548107703afd5248be98f4cc81ae
danieldotnet <d.reust@outlook.com>
2026-09-04 14:44:47 +0200

    Some changes.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
+import string
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+LETTERS = {ord(d): str(i) for i, d in enumerate(string.digits + string.ascii_uppercase)}
 
 
-# Press the green button in the gutter to run the script.
+def _number_iban(iban):
+    return (iban[4:] + iban[:4]).translate(LETTERS)
+
+
+def generate_iban_check_digits(iban):
+    number_iban = _number_iban(iban[:2] + '00' + iban[4:])
+    return '{:0>2}'.format(98 - (int(number_iban) % 97))
+
+
+def valid_iban(iban):
+    return int(_number_iban(iban)) % 97 == 1
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    my_iban = input("Set IBAN: ")  # 'RO13RZBR0000060007134800'
+    my_iban = my_iban.replace(" ", "")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if generate_iban_check_digits(my_iban) == my_iban[2:4] and valid_iban(my_iban):
+        print('IBAN ok!\n')
+    else:
+        print('IBAN not ok!\n')
