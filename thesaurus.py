886dda41e72039329efb70ae89ea799d48207a54
danieldotnet <d.reust@outlook.com>
2026-09-04 15:59:29 +0200

    Some changes.

--- thesaurus.py
+++ thesaurus.py
@@ -2,16 +2,18 @@ import requests
 import streamlit as st
 
 try:
-    term = st.text_input("Bitte Wort eingeben:")
-    url = 'https://www.openthesaurus.de/synonyme/search?q=' + term + '&format=application/json'
-
-    wjdata = requests.get(url).json()
-
     st.title('Thesaurus')
 
-    for i in wjdata["synsets"][0]["terms"]:
-        st.write(i["term"])
-    print('')
+    term = st.text_input("Bitte Wort eingeben:")
+
+    if st.button("Prüfen"):
+        url = 'https://www.openthesaurus.de/synonyme/search?q=' + term + '&format=application/json'
+
+        wjdata = requests.get(url).json()
+
+        for i in wjdata["synsets"][0]["terms"]:
+            st.write(i["term"])
+        print('')
 
 except:
     pass
